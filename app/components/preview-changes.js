976a4d9878d7b64e01332d0f58831bd2a5e4ec93
Heather Brysiewicz <h.a.brysiewicz@gmail.com>
2016-07-03 10:39:13 -0700

    Add save/rollback

--- app/components/preview-changes.js
+++ app/components/preview-changes.js
@@ -1,4 +1,17 @@
 import Ember from 'ember';
 
+const {
+  get
+} = Ember;
+
 export default Ember.Component.extend({
+  actions: {
+    save() {
+      return get(this, 'content').save();
+    },
+
+    rollback() {
+      return get(this, 'content').rollback();
+    }
+  }
 });
